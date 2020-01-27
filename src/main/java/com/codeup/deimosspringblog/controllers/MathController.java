2a332f33ad278a566805006cf3327ad2fa002230
jwhester <jameshester88@gmail.com>
2020-01-27 11:15:35 -0600

    done with the 1st exercise

---
+++ src/main/java/com/codeup/deimosspringblog/controllers/MathController.java
@@ -0,0 +1,31 @@
+package com.codeup.deimosspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public Integer addition(@PathVariable int num1, @PathVariable int num2){
+        return num1 + num2 ;
+    }
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public Integer subtraction(@PathVariable int num1, @PathVariable int num2){
+        return num1 - num2;
+    }
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public Integer multiplication(@PathVariable int num1, @PathVariable int num2){
+        return num1 * num2;
+    }
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public int division(@PathVariable int num1, @PathVariable int num2) {
+        return num1 / num2;
+    }
+}
---
+++ src/main/java/com/codeup/deimosspringblog/controllers/PostController.java
@@ -0,0 +1,32 @@
+package com.codeup.deimosspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postsIndex(){
+        return "Posts index page";
+    }
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postsId(@PathVariable int id){
+        return "View an individual post from id #" + id + ".";
+    }
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postsCreateForm(){
+        return "View the form for creating a post";
+    }
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postsCreate(){
+        return "Create a new post";
+    }
+}
